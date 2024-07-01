c71a970c6d8f2f5504929ffe15b4168a421ae911
stewart.ainsworth@ivendi.com <stewart.ainsworth@ivendi.com>
2024-07-01 22:03:59 +0100

    simple checkout for standard pricing

--- checkout-kata-tests/ServiceTests/CheckoutServiceTests.cs
+++ checkout-kata-tests/ServiceTests/CheckoutServiceTests.cs
@@ -21,4 +21,42 @@ public class CheckoutServiceTests
         result.Should().Be(0);
 
     }
+
+    [Fact]
+    public void When_OneOfEachValidSkuInBasket_ThenExpectedPrice_110()
+    {
+        // ARRANGE
+     
+        // default behaviour : Unknown price
+        _priceRepository.Setup(x => x.GetStockItemRecord(It.IsAny<string>())).Returns((StockItemRecord)null);
+        
+        _priceRepository.Setup(x => x.GetStockItemRecord("A")).Returns(RecordA);
+        _priceRepository.Setup(x => x.GetStockItemRecord("B")).Returns(RecordB);
+        _priceRepository.Setup(x => x.GetStockItemRecord("C")).Returns(RecordC);
+        _priceRepository.Setup(x => x.GetStockItemRecord("D")).Returns(RecordD);
+
+        var checkout = new CheckoutService(_priceRepository.Object);
+
+        // ACT
+
+        checkout.Scan("A");
+        checkout.Scan("B");
+        checkout.Scan("C");
+        checkout.Scan("D");
+
+        var cost = checkout.GetTotalPrice();
+
+        // ASSERT
+
+        cost.Should().Be(110);
+    }
+
+    #region data
+
+    private static StockItemRecord RecordA = new StockItemRecord { Sku = "A", Price = 50 };
+    private static StockItemRecord RecordB = new StockItemRecord { Sku = "B", Price = 30 };
+    private static StockItemRecord RecordC = new StockItemRecord { Sku = "C", Price = 20 };
+    private static StockItemRecord RecordD = new StockItemRecord { Sku = "D", Price = 10 };
+
+    #endregion
 }
--- checkout-kata/Services/CheckoutService.cs
+++ checkout-kata/Services/CheckoutService.cs
@@ -3,6 +3,7 @@
 public class CheckoutService : ICheckoutService
 {
     private readonly IPriceRepository _priceRepository;
+    private readonly List<string> _basket = new();
 
     public CheckoutService(IPriceRepository priceRepository)
     {
@@ -11,11 +12,16 @@ public class CheckoutService : ICheckoutService
 
     public int GetTotalPrice()
     {
-        return 0;
+        var runningTotal = 0;
+     
+        foreach (var itemGroup in _basket.GroupBy(x => x).Select(x => (x.Key, x.Count())))
+            runningTotal += _priceRepository.GetStockItemRecord(itemGroup.Key)?.Price ?? 0 * itemGroup.Item2;
+
+        return runningTotal;
     }
 
     public void Scan(string item)
     {
-        throw new NotImplementedException();
+        _basket.Add(item);
     }
 }
